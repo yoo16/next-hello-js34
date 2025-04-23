8f012d2ab981bdb9d7e7f737cfefb672f9b485e5
yoo16 <yohei.yoshikawa@gmail.com>
2025-04-23 11:41:33 +0900

    update

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 export default function Home() {
   return (
     <div>
-      <h1>Hello Next.js</h1>
+      <h1 className="font-bold text-2xl text-center">Hello Next.js</h1>
     </div>
   );
 }
