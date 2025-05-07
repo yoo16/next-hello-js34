4f31f02a23b8d6631d4f434f7d07d91935006b9a
yoo16 <yohei.yoshikawa@gmail.com>
2025-05-07 10:26:08 +0900

    update

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -1,7 +1,17 @@
 import React from 'react'
+import Link from 'next/link'
 
 export default function Header() {
-  return (
-    <div>Header</div>
-  )
+    return (
+        <header className="bg-pink-300 p-4 shadow">
+            <div className="flex items-center justify-between">
+                <h1 className="text-2xl font-bold text-white">HAL Next</h1>
+                <nav className="space-x-4">
+                    <Link href="/" className="text-white hover:underline">Home</Link>
+                    <Link href="/counter" className="text-white hover:underline">Counter</Link>
+                    <Link href="/profile" className="text-white hover:underline">Profile</Link>
+                </nav>
+            </div>
+        </header>
+    )
 }
