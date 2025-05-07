432aa1257ee30b57144f85a71b1b48cac18b4f8b
yoo16 <yohei.yoshikawa@gmail.com>
2025-05-07 10:17:20 +0900

    update

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import "./globals.css";
+import Header from "./components/Header";
 
 export const metadata: Metadata = {
   title: "Hello HAL",
@@ -14,6 +15,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body>
+        <Header />
         {children}
       </body>
     </html>
