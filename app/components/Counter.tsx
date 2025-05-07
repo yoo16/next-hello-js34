617ccdfa1ffc625ea541fdbeabe24b9f39ca00fb
yoo16 <yohei.yoshikawa@gmail.com>
2025-05-07 11:48:39 +0900

    update

--- app/components/Counter.tsx
+++ app/components/Counter.tsx
@@ -6,11 +6,15 @@ import React, { use, useState } from 'react'
 // import { useState } from 'react'
 
 export default function Counter() {
+    // state
     const [count, setCount] = useState(0);
 
     return (
         <div>
             <p className="text-center">{count}</p>
+            <div>
+                <button className="bg-sky-600 px-3 py-2 text-white rounded-xl">+1</button>
+            </div>
         </div>
     )
 }
