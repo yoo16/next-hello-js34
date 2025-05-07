de34109392b93bc951f934f5fcfcdd0456d614ac
yoo16 <yohei.yoshikawa@gmail.com>
2025-05-07 11:50:50 +0900

    update

--- app/components/Counter.tsx
+++ app/components/Counter.tsx
@@ -9,11 +9,19 @@ export default function Counter() {
     // state
     const [count, setCount] = useState(0);
 
+    function handelIcrement() {
+        alert('+1');
+    }
+
     return (
         <div>
             <p className="text-center">{count}</p>
             <div>
-                <button className="bg-sky-600 px-3 py-2 text-white rounded-xl">+1</button>
+                <button
+                    className="bg-sky-600 px-3 py-2 text-white rounded-xl"
+                    onClick={handelIcrement}
+                >+1
+                </button>
             </div>
         </div>
     )
