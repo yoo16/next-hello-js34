e9f1f3424b5250f082ce0c4c9d99dd22b5b1bc31
yoo16 <yohei.yoshikawa@gmail.com>
2025-05-14 11:02:14 +0900

    update

--- app/components/MemoList.tsx
+++ app/components/MemoList.tsx
@@ -1,3 +1,5 @@
+"use client"
+
 import React, { useState } from 'react'
 
 export default function MemoList() {
@@ -9,14 +11,7 @@ export default function MemoList() {
         setText(event.target.value)
         console.log(event.target.value)
     }
-    // 質問項目：change イベントを追加して
-    <input
-        type="text"
-        placeholder="入力してください"
-        value={text}
-        onChange={handleChange}
-        className="border border-gray-300 rounded px-4 py-2 focus:outline-none focus:ring-2 focus:ring-blue-400"
-    />
+
     return (
         <div className="flex justify-center mt-10">
             <div className="flex space-x-4">
@@ -24,6 +19,8 @@ export default function MemoList() {
                     type="text"
                     placeholder="入力してください"
                     className="border border-gray-300 rounded px-4 py-2 focus:outline-none focus:ring-2 focus:ring-blue-400"
+                    onChange={handleChange}
+                    value={text}
                 />
                 <button className="bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600 transition">
                     追加
