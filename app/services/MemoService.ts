ecb774f5478125527f23b75070bf5be2745fc684
yoo16 <yohei.yoshikawa@gmail.com>
2025-05-21 10:55:47 +0900

    update

--- app/services/MemoService.ts
+++ app/services/MemoService.ts
@@ -1,12 +1,16 @@
 export async function addMemo(text: string) {
+    // http://localhost:3000/api/memo/add 
+    // にPOSTリクエスト&レスポンス
     const res = await fetch("/api/memo/add", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({ text }),
     });
     if (!res.ok) {
+        // レスポンスが200 OKでない場合
         return { message: "メモの追加に失敗しました" };
     }
+    // APIサーバからのJSONレスポンスをJavaScriptオブジェクトに変換
     const result = await res.json();
     if (res.ok) {
         return result;
