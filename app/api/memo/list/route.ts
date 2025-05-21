847db40105a39476908b56c7bbed55b625fb7978
yoo16 <yohei.yoshikawa@gmail.com>
2025-05-21 10:21:13 +0900

    update

--- app/api/memo/list/route.ts
+++ app/api/memo/list/route.ts
@@ -3,8 +3,12 @@ import { NextResponse } from 'next/server';
 
 // http://localhost:3000/api/memo/list
 export async function GET() {
+    // dummyデータを作成
+    // TODO: DBから取得するように修正
+    const memos = [ "りんご", "ばなな", "みかん" ];
     const data = {
-        message: "メモ一覧を取得しました"
+        message: "メモ一覧を取得しました",
+        memos: memos,
     }
     return NextResponse.json(data);
 }
