9b1557b39261b1fc99981fdc012608251230144d
yoo16 <yohei.yoshikawa@gmail.com>
2025-05-21 10:18:39 +0900

    update

--- app/api/memo/list/route.ts
+++ app/api/memo/list/route.ts
@@ -1,6 +1,7 @@
 // GETルーティング作成
 import { NextResponse } from 'next/server';
 
+// http://localhost:3000/api/memo/list
 export async function GET() {
     const data = {
         message: "メモ一覧を取得しました"
