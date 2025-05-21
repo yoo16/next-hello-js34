3d9152c36203a53a6a0d577f51cf53e76a65b472
yoo16 <yohei.yoshikawa@gmail.com>
2025-05-21 10:47:33 +0900

    update

--- app/api/memo/add/route.ts
+++ app/api/memo/add/route.ts
@@ -3,15 +3,16 @@ import { NextResponse } from 'next/server';
 
 // http://localhost:3000/api/memo/add
 export async function POST(req: Request) {
-    // TODO: DBまたはファイルに保存
-
     // リクエストからデータを取得
     const { text } = await req.json();
+
+    // TODO: text をDBまたはファイルに保存
 
     // レスポンスデータ作成
     const data = {
         message: "メモを保存しました",
         memo: text,
     }
+    // レスポンスを返す
     return NextResponse.json(data);
 }
