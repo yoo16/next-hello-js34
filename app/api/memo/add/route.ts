a36943b1f2ad9850fbfc5f6932c127dfb95a2b9a
yoo16 <yohei.yoshikawa@gmail.com>
2025-05-28 10:10:42 +0900

    update

--- app/api/memo/add/route.ts
+++ app/api/memo/add/route.ts
@@ -1,4 +1,5 @@
 // POSTルーティング作成
+import { addMemo } from '@/app/repositories/MemoRepository';
 import { NextResponse } from 'next/server';
 
 // http://localhost:3000/api/memo/add
@@ -6,7 +7,8 @@ export async function POST(req: Request) {
     // リクエストからデータを取得
     const { text } = await req.json();
 
-    // TODO: text をDBまたはファイルに保存
+    // TODO: text をRDBまたはファイルに保存
+    await addMemo(text);
 
     // レスポンスデータ作成
     const data = {
