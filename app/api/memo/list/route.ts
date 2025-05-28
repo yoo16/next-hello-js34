6f1dded8193c204ac55fbbfb1f8df7c4a078a1ff
yoo16 <yohei.yoshikawa@gmail.com>
2025-05-28 10:28:34 +0900

    update

--- app/api/memo/list/route.ts
+++ app/api/memo/list/route.ts
@@ -1,11 +1,12 @@
 // GETルーティング作成
+import { getAllMemos } from '@/app/repositories/MemoRepository';
 import { NextResponse } from 'next/server';
 
 // http://localhost:3000/api/memo/list
 export async function GET() {
     // dummyデータを作成
     // TODO: DBから取得するように修正
-    const memos = [ "りんご", "ばなな", "みかん" ];
+    const memos = getAllMemos();
     const data = {
         message: "メモ一覧を取得しました",
         memos: memos,
