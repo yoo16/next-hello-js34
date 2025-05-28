fd2737a1e56eced3953c493f5fe40ea3b32cc0ed
yoo16 <yohei.yoshikawa@gmail.com>
2025-05-28 12:24:59 +0900

    update

--- app/api/gemini/route.ts
+++ app/api/gemini/route.ts
@@ -1,5 +1,6 @@
 import { NextRequest, NextResponse } from "next/server";
 
+// http://localhost:3000/api/gemini
 export async function GET(req: NextRequest) {
     const API_KEY = process.env.GEMINI_API_KEY;
     if (!API_KEY) return;
