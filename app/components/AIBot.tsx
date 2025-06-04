e65f052061f6cf87df7daba4ba3b3e503150dfc3
yoo16 <yohei.yoshikawa@gmail.com>
2025-06-04 10:37:12 +0900

    update

--- app/components/AIBot.tsx
+++ app/components/AIBot.tsx
@@ -3,6 +3,14 @@ import React from 'react'
 
 export default function AIBot() {
     return (
-        <div>AIBot</div>
+        <div className="mx-auto max-w-2xl p-6 bg-white rounded-lg shadow-md space-y-4">
+            <h2 className="text-2xl font-bold">教えて！Gemini!</h2>
+            <div className="text-left p-3 bg-gray-100 rounded shadow text-sm whitespace-pre-wrap">
+            </div>
+            <button
+                className="px-6 py-3 bg-sky-600 text-white rounded-xl shadow hover:bg-sky-700 cursor-pointer">
+                教えて!Gemini!
+            </button>
+        </div>
     )
 }
