edb8656feb060d0131ce3a07755ef930a0617d39
yoo16 <yohei.yoshikawa@gmail.com>
2025-06-04 10:16:26 +0900

    update

--- app/api/gemini/route.ts
+++ app/api/gemini/route.ts
@@ -1,9 +1,13 @@
+import { GoogleGenAI } from "@google/genai";
 import { NextRequest, NextResponse } from "next/server";
 
 // http://localhost:3000/api/gemini
 export async function GET(req: NextRequest) {
     const API_KEY = process.env.GEMINI_API_KEY;
     if (!API_KEY) return;
+
+    // Google GenAI を生成
+    const ai = new GoogleGenAI({ apiKey: API_KEY });
 
     const data = { message: API_KEY };
     return NextResponse.json(data);
